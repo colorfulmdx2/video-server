96f5f0b2b74d777c9b2dd79e9401dea2c999c44f
Viktor <colorfulmdx2@gmail.com>
2022-03-31 20:09:59 +0300

    init commit

---
+++ src/index.ts
@@ -0,0 +1,34 @@
+import express, { Request, Response } from 'express'
+import cors from 'cors'
+import bodyParser from "body-parser";
+import swaggerUi from 'swagger-ui-express'
+import swaggerDocument from '../swagger.json'
+import {router} from './router'
+
+export let videos = [
+    {id: 1, title: 'About JS - 01', author: 'it-incubator.eu'},
+    {id: 2, title: 'About JS - 02', author: 'it-incubator.eu'},
+    {id: 3, title: 'About JS - 03', author: 'it-incubator.eu'},
+    {id: 4, title: 'About JS - 04', author: 'it-incubator.eu'},
+    {id: 5, title: 'About JS - 05', author: 'it-incubator.eu'},
+]
+
+const app = express()
+const port = 5000
+
+app.use(cors())
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded())
+app.use(
+    '/api-docs',
+    swaggerUi.serve,
+    swaggerUi.setup(swaggerDocument)
+);
+app.use('/lesson_01/api', router);
+app.all('*', function(req, res) {
+    res.send(400)
+})
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+})
---
+++ src/router.ts
@@ -0,0 +1,72 @@
+import express, { Request, Response } from 'express'
+export const router = express.Router()
+
+export let videos = [
+    {id: 1, title: 'About JS - 01', author: 'it-incubator.eu'},
+    {id: 2, title: 'About JS - 02', author: 'it-incubator.eu'},
+    {id: 3, title: 'About JS - 03', author: 'it-incubator.eu'},
+    {id: 4, title: 'About JS - 04', author: 'it-incubator.eu'},
+    {id: 5, title: 'About JS - 05', author: 'it-incubator.eu'},
+]
+
+export let bloggers = [
+    {id: 1, name: 'Klava Koka', author: 'youtubeUrl'},
+    {id: 1, name: 'Klava Koka', author: 'youtubeUrl'},
+]
+
+router.use(function timeLog(req, res, next) {
+    console.log('Time: ', Date.now());
+    next();
+});
+
+router.get('/get-all-videos', function(req, res) {
+   res.send(videos);
+});
+
+router.post('/upload-video', (req: Request, res: Response) => {
+    const newVideo = {
+        id: +(new Date()),
+        title: req.body.title,
+        author: 'it-incubator.eu'
+    }
+    videos.push(newVideo)
+    res.status(201).send(videos)
+})
+
+router.get('/get-video/:id', (req: Request, res: Response) => {
+    if (!req.params.id) {
+        res.status(400).send(`Bad request`)
+    }
+    const find = videos.find((e) => e.id === +req.params.id)
+    if (!find) {
+        res.status(404).send({message: `Video not found`})
+    }
+    res.status(200).send(find)
+})
+
+router.delete('/delete-video/:id', (req: Request, res: Response) => {
+    const id = +req.params.id
+    const find = videos.find((e) => e.id === id)
+    if (!find) {
+        res.status(404).send({message: `Video not found`})
+    }
+    videos = [...videos.filter((e: any) => e.id !== +req.params.id)]
+    res.status(200).send(find)
+})
+
+router.put('/update-video/:id', (req: Request, res: Response) => {
+    const id = +req.params.id
+    const find = videos.find((e) => e.id === id)
+    if (!find) {
+        res.status(404).send({message: `Video not found`})
+    }
+    const { title }: any = req.body
+    videos = videos.map((e: any) => {
+        if (+id === e.id) {
+            return {...e, title}
+        }
+        return e
+    })
+    res.send(200)
+})
+
